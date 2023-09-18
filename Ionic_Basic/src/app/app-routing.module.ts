437ffc647e8f197654791c3e0a0f7a7f8abef469
Samuel Kono <35778636+Samysuper14@users.noreply.github.com>
2023-09-17 22:09:11 -0600

    Practica 5
    Practica 5 terminada, las recetas aparecen pero no pude lograr que se desplazaran

--- Ionic_Basic/src/app/app-routing.module.ts
+++ Ionic_Basic/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'inicio',
+    redirectTo: 'receta',
     pathMatch: 'full'
   },
   {
@@ -27,6 +27,15 @@ const routes: Routes = [
     path: 'alumnos',
     loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
+  {
+    path: 'detalle-receta',
+    loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
+  },
+  {
+    path: 'receta',
+    loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
+  },
+
 
 
 ];
