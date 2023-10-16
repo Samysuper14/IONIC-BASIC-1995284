2f6315be4f89b49772a567cc0a61c95b8e8e91bb
Samuel Kono <35778636+Samysuper14@users.noreply.github.com>
2023-10-15 21:39:52 -0600

    Practica 9
    Agregamos la tab de destinos e hicimos el interface de lugar

--- Ionic_Basic/src/app/app-routing.module.ts
+++ Ionic_Basic/src/app/app-routing.module.ts
@@ -55,7 +55,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },  {
+    path: 'destinos',
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
   },
+
 
 
 
